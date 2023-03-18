afdde50a27344e5ccaa1c04732b213aa1eb2dc4c
HawemoX Delaine <99209498+HawemoX@users.noreply.github.com>
2023-03-18 17:20:46 +0100

    Delete NewTab directory

--- NewTab/script.js
+++
@@ -1,9 +0,0 @@
-function displayTime(){
-  var dateTime = new Date();
-  var hrs = dateTime.getHours();
-  var min = dateTime.getMinutes();
-
-  document.getElementById('hours').innerHTML = hrs;
-  document.getElementById('minutes').innerHTML = min;
-}
-setInterval(displayTime, 10);
